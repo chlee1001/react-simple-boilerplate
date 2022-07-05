9005e18b9cdf92ff90da0fd6f847dfebb9163309
chlee1001 <chlee1001@naver.com>
2022-07-05 14:02:33 +0900

    :lipstick: styled-components 코드 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-
+import styled from 'styled-components'
 import styles from './App.module.css'
+
+const StyledButton = styled.button`
+  padding: 0.375rem 0.75rem;
+  border-radius: 0.25rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  border: 1px solid lightgray;
+  color: gray;
+`
 
 function App() {
   return (
     <div className={styles.AppContainer}>
       <h1>simple-react-boilerplate</h1>
+      <StyledButton className={styles.button}>버튼</StyledButton>
     </div>
   )
 }
